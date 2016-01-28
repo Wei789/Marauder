17a6d2a7c075f014269eaedc9e1ad2d59f71ea04
Wei Chen <Wei_Cheng@migocorp.com>
2016-01-28 15:33:13 +0800

    no message

---
+++ src/Marauder.Task/Job/BaseJob.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+
+namespace Marauder.Task.Job
+{
+    public class BaseJob
+    {
+        protected readonly Logger Log;
+
+        public BaseJob()
+        {
+            this.Log = LogManager.GetLogger(this.GetType().Name);
+        }
+    }
+}
