cbe81213aace404f7c101513a0d942603df1c578
Wei Chen <Wei_Cheng@migocorp.com>
2016-01-30 17:00:36 +0800

    no message

--- src/Marauder.Web/Controllers/Api/ValueController.cs
+++ src/Marauder.Web/Controllers/Api/ValueController.cs
@@ -1,14 +1,6 @@
 ﻿using Marauder.BLL.ViewModels;
 using Marauder.Help;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
 using System.Web.Http;
 
 namespace Marauder.Web.Controllers.Api
